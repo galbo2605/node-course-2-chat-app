d5edab55c3053b4fae573c1d2f99ccc9b2135ca9
galbo2605 <gal2605@gmail.com>
2018-01-26 20:06:47 +0200

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,7 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
-socket.on('newLocationMessage',function (message) {
+socket.on('newLocationMessage', function (message) {
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
@@ -28,27 +28,33 @@ socket.on('newLocationMessage',function (message) {
 
 jQuery('#message-from').on('submit', function (e) {
     e.preventDefault();
+
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
 var locationButton = jQuery('#send-location');
-
 locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
